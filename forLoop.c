432371f0e93a6e96943f6553f4b8745000aed604
oiacow <oiacow@gmail.com>
2019-03-27 18:47:41 -0300

    commits

--- forLoop.c
+++ forLoop.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-    int i, numberOfNums=0, total=0, value;
+    int i, nRead, numberOfNums=0, total=0, values;
     float average;
 
-    printf("How many numbers are to be read?\n");
+    printf("How many numbers you want to read?\n");
     scanf("%d", &numberOfNums);
     
-    valsead = scanf("%d", &value);    
+    nRead = scanf("%d", &values);    
 
     for(i=0; i<numberOfNums; i++) {
-    scanf("%d", &value);
-    total += value; //same as total = total + value;
-    printf("Read %d\n", value);
+    scanf("%d", &values);
+    total += values; //same as total = total + value;
+    printf("Read %d\n", values);
     }
     average = (float)total / numberOfNums;
         if (average != (float)total)
-        printf("you are too dumb!\n");
+        printf("exceeded the maximum length!\n");
         
     else
     printf("You read %d values . Average = %f.\n", numberOfNums, average);    
